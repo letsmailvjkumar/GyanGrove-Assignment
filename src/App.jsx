ed7f84ada01cf567bf3d997f1edfb2bfc26856f7
VJKUMAR <115359380+letsmailvjkumar@users.noreply.github.com>
2024-03-30 00:24:11 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+  
+
+  return (
+    <>
+    <div className='mx-10 my-5 max-[480px]:mx-auto'>
+      <Navbar/>
+    </div>
+    </>
+  )
+}
+
+export default App

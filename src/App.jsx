191f0481d13010828115c97a74ed647faab9a250
VJKUMAR <115359380+letsmailvjkumar@users.noreply.github.com>
2024-03-30 19:13:57 +0530

    desktop-view

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import './App.css'
+import Banner from './components/Banner'
+import Events from './components/Events'
 import Navbar from './components/Navbar'
+import Shows from './components/Shows'
 
 function App() {
   
 
   return (
     <>
-    <div className='mx-10 my-5 max-[480px]:mx-auto'>
+    <div className='mx-10 my-5 max-[480px]:mx-2'>
       <Navbar/>
+    </div>
+    <div>
+        <Banner />
+    </div>
+    <div className="mt-10 mx-20">
+        <Shows />
+      </div>
+    <div className="mt-10 mx-20">
+      <Events />
     </div>
     </>
   )
